0da5526fc143e4d9b0075b646d31132032dd084a
Andrew TOwnsley <atownsley86@gmail.com>
2021-04-23 23:49:30 -0600

    Javascript Functionality COmplete.  COme Back later and rewrite it

--- app.js
+++ app.js
@@ -42,15 +42,71 @@ class UI {
 
     list.appendChild(row);
   }
+
+  static deleteBook(el) {
+    if(el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    };
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    // Make alert disappear after 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+  }
 }
 
 // Store Classs: Handles Storage
 
 // Event: Display Books
-
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // prevent default form submit
+  e.preventDefault();
+  // Get Form Values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // Validate
+  if(title === '' || author === '' || isbn === '') {
+    UI.showAlert('Please fill out all of the Fields', 'danger');
+  } else {
+  // Instantiate a book
+  const book = new Book(title, author, isbn);
+  // Add Book to UI
+  UI.addBookToList(book);
+  
+//SHow success message
+  UI.showAlert('Book Added', 'success');
+
+  // Clear Fields
+  UI.clearFields();
+  }
+
+
+});
+
 
 //Event: Remove a Book;
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  console.log(e.target);
+  UI.deleteBook(e.target);
+
+  // Book Removed
+  UI.showAlert('Book Removed', 'success');
+})
 
